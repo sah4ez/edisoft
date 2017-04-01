9bf9ac0efaf159a16edcc850c7692dd63365c218
Alexandr <sah4ez32@gmail.com>
2017-04-01 21:24:37 +0300

    Added several tests.

--- tree/src/test/java/com/github/sah4ez/UtilsTest.java
+++ tree/src/test/java/com/github/sah4ez/UtilsTest.java
@@ -9,19 +9,23 @@ import static org.junit.Assert.*;
  * Created by sah4ez on 01.04.17.
  */
 public class UtilsTest {
-    private Node<Integer> root;
-    @Before
-    public void setUp() throws Exception {
-        root = new Node<Integer>(1);
-        Node<Integer> child1 = new Node<Integer>(1);
-        Node<Integer> child2 = new Node<Integer>(1);
-        Node<Integer> child3 = new Node<Integer>(1);
-        Node<Integer> child4 = new Node<Integer>(1);
-        Node<Integer> child5 = new Node<Integer>(1);
-        Node<Integer> child6 = new Node<Integer>(1);
-        Node<Integer> child7 = new Node<Integer>(1);
+    Node<Integer> root = new Node<Integer>(1);
+    Node<Integer> child1 = new Node<Integer>(1);
+    Node<Integer> child2 = new Node<Integer>(1);
+    Node<Integer> child3 = new Node<Integer>(1);
+    Node<Integer> child4 = new Node<Integer>(1);
+    Node<Integer> child5 = new Node<Integer>(1);
+    Node<Integer> child6 = new Node<Integer>(1);
+    Node<Integer> child7 = new Node<Integer>(1);
 
+    @Test
+    public void testZeroHeight() throws Exception {
+        Node<Integer> empty = new Node<Integer>(0);
+        assertEquals(0, Utils.height(empty));
+    }
 
+    @Test
+    public void heightRootWithTwoNodes() throws Exception {
         root.addChild(child1);
         root.addChild(child2);
 
@@ -32,16 +36,32 @@ public class UtilsTest {
         child2.addChild(child6);
 
         child6.addChild(child7);
-    }
-
-    @Test
-    public void testZeroHeight() throws Exception {
-        Node<Integer> empty = new Node<Integer>(0);
-        assertEquals(0, Utils.height(empty));
-    }
-
-    @Test
-    public void testHeight() throws Exception {
         assertEquals(3, Utils.height(root));
+    }
+
+    @Test
+    public void heightRootWithOneNode() throws Exception {
+        root.addChild(child1);
+        child1.addChild(child2);
+        child2.addChild(child3);
+        child3.addChild(child4);
+        child4.addChild(child5);
+        child5.addChild(child6);
+        child6.addChild(child7);
+
+        assertEquals(7, Utils.height(root));
+    }
+
+    @Test
+    public void heightWithSixNodes() throws Exception {
+        root.addChild(child1);
+        root.addChild(child2);
+        root.addChild(child3);
+        root.addChild(child4);
+        root.addChild(child5);
+        root.addChild(child6);
+        child6.addChild(child7);
+
+        assertEquals(2, Utils.height(root));
     }
 }
